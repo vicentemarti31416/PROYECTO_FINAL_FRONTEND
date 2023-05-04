c962b091a8fe2db17dc014909f24953da129022f
Agustin Caldera <agus@Agustin-2.local>
2023-05-04 22:21:43 +0200

    inicial

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HomePage from "./Pages/Homepage/HomePage";
+
+function App() {
+  return (
+    <div>
+    <HomePage></HomePage>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Homepage/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './HomePage.css'
+
+const HomePage = () => {
+
+
+
+  return (
+    <div className='div'>
+      PERRO
+      <button className="button"></button>
+    </div>
+  )
+}
+
+export default HomePage
