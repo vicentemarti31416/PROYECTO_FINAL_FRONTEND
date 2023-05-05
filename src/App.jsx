3b57bd98215d7dd672ae7d775ab2c55d99ef00d9
Haruuth <haruuth@gmail.com>
2023-05-05 16:50:19 +0200

    ordenacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,18 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios"
-
-import { CandidatesDetails } from "./Components/CandidatesDetails/CandidateDetails";
-import { OffersDetails } from "./Components/OffersDetails/OffersDetails";
-import { CandidatesPage } from "./Pages/CandidatesPage/CandidatesPage";
-import { OffersPage } from "./Pages/OffersPage/OffersPage";
-import HomePage from "./Pages/HomePage/HomePage";
-import Nav from "./Pages/nav/Nav";
 import React from "react";
-import LoginPage from "./Pages/Login/LoginPage";
-import HomePage from "./Pages/Homepage/HomePage";
-import {Congrats} from "./Components/Congrats/Congrats";
+import Welcome from "./Components/Welcome/Welcome";
 
 function App() {
-  const [candidates, setCandidates] = useState([]);
-  const [offers, setOffers] = useState([]);
 
-  const getCandidates = () => {
-    axios
-    .get("http://localhost:8000/candidates")
-    .then((res) => {
-      setCandidates(res.data);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-};
-
-  const getOffers = () => {
-    axios
-    .get("http://localhost:8000/offers")
-    .then((res) => {
-      setOffers(res.data);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-};
-
-
-  
-
-  useEffect (() =>{
-    getCandidates()
-    getOffers()
-  }, []);
 
   return (
     <div>
-    <Congrats></Congrats>
-    <LoginPage></LoginPage>
-    <CandidatesDetails candidates={candidates}></CandidatesDetails>
-    <OffersDetails offers={offers}></OffersDetails>
     <Welcome></Welcome>
-    <HomePage></HomePage>
-    <Nav></Nav>
-    <CandidatesPage candidates={candidates}></CandidatesPage>
-    <OffersPage offers={offers}></OffersPage>
+    {/* <Congrats></Congrats> */}
+    {/* <LoginPage></LoginPage> */}
+    {/* <HomePage offers={offers} candidates={candidates}></HomePage> */}
+    {/* <Nav></Nav> */}
+    {/* <CandidatesPage candidates={candidates}></CandidatesPage>
+    <OffersPage offers={offers}></OffersPage> */}
     </div>
   );
 }
