7ec4490a8f5d0a123d0fe63ed49734d24c2ea77c
Lur81 <l.cano.soler@gmail.com>
2023-05-05 14:54:22 +0200

    Welcme component-slider

--- src/Components/Welcome/Welcome.jsx
+++ src/Components/Welcome/Welcome.jsx
@@ -1,37 +1,51 @@
-import React, { useRef, useState } from "react";
-// Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
-
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";// Import Swiper React components
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
-
 import "./Welcome.css";
-
-// import required modules
-import { Pagination } from "swiper";
-
-
-
-
-
-
-
+import { Pagination } from "swiper"; // import required modules
+import group7 from '../../assets/group7.png'
 
 export default function Welcome() {
-  return (
-    <>
-      <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-      </Swiper>
-    </>
-  );
+
+    const [clickCount, setClickCount] = useState(0);
+
+    const handleClick = () => {
+        setClickCount(clickCount + 1);
+    };
+    return (
+        <>
+            <div className="container">
+                <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
+
+                    <SwiperSlide>
+                        <div className="" >
+                            <img alt="group7" src={group7} />
+                        </div>
+                        <div className="" >
+                            <p>Nos encanta verte por</p>
+                            <img alt="group7" src={group7} />
+                        </div>
+                        <div className="" >
+                            <p>Atrae y enamora a los mejores candidatos</p>
+                        </div>
+                        <div>
+                            <button onClick={handleClick}>Haz clic aquí</button>
+                            <p>Has hecho clic {clickCount} veces.</p>
+                        </div>
+                    </SwiperSlide>
+
+                    <SwiperSlide>Slide 2</SwiperSlide>
+                    <SwiperSlide>Slide 3</SwiperSlide>
+                    <SwiperSlide>Slide 4</SwiperSlide>
+                    <SwiperSlide>Slide 5</SwiperSlide>
+                    <SwiperSlide>Slide 6</SwiperSlide>
+                    <SwiperSlide>Slide 7</SwiperSlide>
+                    <SwiperSlide>Slide 8</SwiperSlide>
+                    <SwiperSlide>Slide 9</SwiperSlide>
+                </Swiper>
+            </div>
+        </>
+    );
 }
