01dddf63ac5cc12d531825a201274ebd92dae6fc
Haruuth <haruuth@gmail.com>
2023-05-05 14:01:21 +0200

    datos pintados

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,47 @@
-import React from "react";
-import HomePage from "./Pages/Homepage/HomePage";
+import React, { useState, useEffect } from "react";
+import axios from "axios"
+import { CandidatesPage } from "./Pages/CandidatesPage/CandidatesPage";
+import { OffersPage } from "./Pages/OffersPage/OffersPage";
+
 
 function App() {
+  const [candidates, setCandidates] = useState([]);
+  const [offers, setOffers] = useState([]);
+
+  const getCandidates = () => {
+    axios
+    .get("http://localhost:8000/candidates")
+    .then((res) => {
+      setCandidates(res.data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+  const getOffers = () => {
+    axios
+    .get("http://localhost:8000/offers")
+    .then((res) => {
+      setOffers(res.data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+
+  
+
+  useEffect (() =>{
+    getCandidates()
+    getOffers()
+  }, []);
+
   return (
     <div>
-    <HomePage></HomePage>
+    <CandidatesPage candidates={candidates}></CandidatesPage>
+    <OffersPage offers={offers}></OffersPage>
     </div>
   );
 }
