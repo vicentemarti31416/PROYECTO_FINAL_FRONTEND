29d029b7c2b4252784beab419816b634fd98a5c2
Lur81 <l.cano.soler@gmail.com>
2023-05-05 14:25:30 +0200

    Welcme component-slider

--- src/Pages/Homepage/HomePage.jsx
+++ src/Pages/Homepage/HomePage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './HomePage.css'
+import Welcome from '../../Components/Welcome/Welcome'
 
 const HomePage = () => {
 
@@ -8,6 +9,7 @@ const HomePage = () => {
   return (
     <div className='div'>
       PERRO
+      <Welcome></Welcome>
       <button className="button"></button>
     </div>
   )
