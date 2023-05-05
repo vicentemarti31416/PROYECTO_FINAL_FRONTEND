8db8b1bfd7ad5acf3589e1fcc9f2b23e4bf52d79
TIN <escuderotin@gmail.com>
2023-05-05 14:30:14 +0200

    fallo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import HomePage from "./Pages/Homepage/HomePage";
+import HomePage from "./Pages/HomePage/HomePage";
+import Nav from "./Pages/nav/Nav";
+
 
 function App() {
   return (
     <div>
     <HomePage></HomePage>
+    <Nav></Nav>
     </div>
   );
 }
