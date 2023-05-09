d12e697da951f95c9000f4a3356c292e83b1aa88
Lur81 <l.cano.soler@gmail.com>
2023-05-09 10:29:46 +0200

    modal filter

--- src/Components/Searcher/Searcher.jsx
+++ src/Components/Searcher/Searcher.jsx
@@ -1,13 +1,19 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { SearchContext } from "../../App";
 import { useLocation } from "react-router-dom";
+import { FilterModal } from "../FilterModal/FilterModal";
 
 export default function Searcher() {
   const location = useLocation();
   console.log(location.pathname);
 
   const { searchText, setSearchText } = useContext(SearchContext);
-
+  const [open, setOpen] = useState(false);
+  
+  const handleFilterClick = () => {
+    setOpen(true);
+  };
+  
   useEffect(() => {
     setSearchText("");
   }, [location.pathname]);
@@ -17,14 +23,18 @@ export default function Searcher() {
       {(location.pathname === "/candidates" ||
         location.pathname === "/offers" ||
         location.pathname === "/candidates") && (
-        <input
-          className="dentro-buscador"
-          type="text"
-          id="buscar"
-          placeholder="Buscar"
-          onChange={(e) => setSearchText(e.target.value)}
-        />
-      )}
+          <input
+            className="dentro-buscador"
+            type="text"
+            id="buscar"
+            placeholder="Buscar"
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+        )}
+      <div>
+        <button onClick={handleFilterClick} className=''>Filtro</button>
+        <FilterModal isOpen={open} />
+      </div>
     </>
   );
 }
